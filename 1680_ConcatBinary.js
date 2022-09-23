dc28267ab484bdb8df746dc0119aae3ba5176ed6
Jeong Hun Nam <njh3421@gmail.com>
2022-09-23 12:12:25 +0900

    Add Testcase

--- 1680_ConcatBinary.js
+++ 1680_ConcatBinary.js
@@ -2,12 +2,17 @@
 Given an integer n, return the decimal value of the binary string formed by concatenating the binary representations of 1 to n in order, modulo 109 + 7.
 modulo인 10^9+7로 나눠주는 이유는 수의 표현이 제한적이기 때문에
 답을 도출해낼 수 있는지 알고싶기에 모듈러 연산을 사용한다
+
+ex) 3 -> 27
+1 -> 1
+2 -> 10
+3 -> 11 => 11011를 10진수로 바꾸면 27
+
+10보다 커지면 e단위가 나오기에 modulo로 나눠줘서 답을 도출
  */
 var concatenatedBinary = function(n) {
   let modulo=1e9+7
   let num=0
-  // console.log((n.toString(2)));
-  // console.log(2**n.toString(2).length);
   for (let i = 0; i <=n; i++) {
     num*=(2**i.toString(2).length)
     num+=i
