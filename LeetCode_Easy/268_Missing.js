559515d7095543f847e877b9536710385750d84e
pydevjeong <njh3421@gmail.com>
2022-10-06 21:33:59 +0900

    268. Missing Number
    Easy

---
+++ LeetCode_Easy/268_Missing.js
@@ -0,0 +1,11 @@
+var missingNumber = function(nums) {
+  nums.sort((a,b)=>a-b)
+  console.log(nums);
+  nums.forEach(e=>{
+      if(count===e){
+          count++
+      }
+  })
+  return count
+};
+missingNumber([45,35,38,13,12,23,48,15,44,21,43,26,6,37,1,19,22,3,11,32,4,16,28,49,29,36,33,8,9,39,46,17,41,7,2,5,27,20,40,34,30,25,47,0,31,42,24,10,14])
