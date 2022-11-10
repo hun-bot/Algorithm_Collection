31d1b8e089b20cd5515631d568ad6011c0dac472
Jeong Hun Nam <njh3421@gmail.com>
2022-11-10 20:53:28 +0900

    Medium,Easy

---
+++ LeetCode_Medium/142_LinkedListCycle2.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var detectCycle = function(head) {
+  let slow=head
+  let fast=head
+  while(fast&&fast.next){
+      slow=slow.next
+      fast=fast.next.next
+      if(slow===fast) break
+  }
+  if(!fast||!fast.next) return null
+  let cur = head;
+  while(cur !== fast) {
+      cur = cur.next;
+      fast = fast.next;
+  }
+  return cur
+}
