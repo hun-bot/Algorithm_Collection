bb6b4856922922eb6c568ebc24e2a75c674f8ce2
pydevjeong <njh3421@gmail.com>
2022-12-19 20:18:39 +0900

    Solved

--- Programmers/LV1/StringSplit.js
+++ Programmers/LV1/StringSplit.js
@@ -1,48 +1,21 @@
 /* 문자열 나누기 https://school.programmers.co.kr/learn/courses/30/lessons/140108 */
 
-let count = 0;
 function solution(s) {
-  let lenArr = [];
-  let xCount = 0;
-  let notXcount = 0;
-  let sArr = s.split("");
-  while (sArr.length > 0) {
-    checkArr(sArr,lenArr,xCount,notXcount)
-    if(sArr.length===1) {
+  let x;
+  let xCount=0
+  let notXcount=0
+  let count = 0;
+  for (const i of s) {
+    if(!x) x=i
+    if(x===i) xCount++;
+    else notXcount++;
+    if(xCount===notXcount){
       count++
-      break
-    }
-    if(count===0){
-      count++
-      break
+      xCount=0
+      notXcount=0
+      x=""
     }
   }
-  return count
+  if(x) count++
+  return x
 }
-
-const checkArr=(sArr,lenArr,xCount,notXcount)=>{
-  for (let i=0;i<sArr.length;i++) {
-    let x = sArr[0];
-    if (sArr[i] === x) {
-      xCount += 1;
-    } else {
-      notXcount += 1;
-    }
-    if (xCount === notXcount) {
-      helper(sArr, lenArr, xCount + notXcount);
-      xCount = 0;
-      notXcount = 0;
-    }
-  }
-}
-const helper = (sArr, lenArr, hap) => {
-  for (let i = 0; i < hap; i++) {
-    lenArr.push(sArr.shift());
-  }
-  count++
-};
-
-// solution("banana");
-// solution("qqqqdddwwwwcccc")
-// solution("abracadabra")
-// solution("aaabbaccccabba")
