ae95a0cfdcd71cd113b64603c47d1d6daad95d2d
pydevjeong <njh3421@gmail.com>
2022-12-20 23:50:29 +0900

    Sovling Lv1 Trio

---
+++ Programmers/LV1/Trio.js
@@ -0,0 +1,8 @@
+/* 삼총사 https://school.programmers.co.kr/learn/courses/30/lessons/131705 */
+
+function solution(number) {
+  let sortedNumber=number.sort()
+  console.log(sortedNumber)
+}
+
+solution([-2, 3, 0, 2, -5])
