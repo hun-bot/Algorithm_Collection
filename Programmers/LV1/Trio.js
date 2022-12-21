11a903a6b5507bcb0e7b133ce1c69c5170b20881
pydevjeong <njh3421@gmail.com>
2022-12-21 16:22:34 +0900

    using three for loop

--- Programmers/LV1/Trio.js
+++ Programmers/LV1/Trio.js
@@ -1,8 +1,12 @@
 /* 삼총사 https://school.programmers.co.kr/learn/courses/30/lessons/131705 */
 
 function solution(number) {
-  let sortedNumber=number.sort()
-  console.log(sortedNumber)
+  let sum = 0;
+  for (let i = 0; i < number.length; i++) {
+    for (let j = i + 1; j < number.length; j++) {
+      for (let k = j + 1; k < number.length; k++)
+        if (number[i] + number[j] + number[k] === 0) sum += 1;
+    }
+  }
+  return sum;
 }
-
-solution([-2, 3, 0, 2, -5])
