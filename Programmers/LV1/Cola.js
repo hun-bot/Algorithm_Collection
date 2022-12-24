03852db8768f5b75bcf01e0dd35b9007ff335063
pydevjeong <njh3421@gmail.com>
2022-12-24 22:51:17 +0900

    Solved Cola

--- Programmers/LV1/Cola.js
+++ Programmers/LV1/Cola.js
@@ -1,18 +1,10 @@
 /* 콜라 문제 https://school.programmers.co.kr/learn/courses/30/lessons/132267 */
 function solution(a, b, n) {
-  let cola=[]
-  let remainCola=[]
+  let res=0
   while(n>=a){
-    cola.push(Math.floor(n/a)*b)
-    if(!(n%a<1)) remainCola.push(n%a)
-    n=n/a
-    console.log(cola,remainCola);
+    res+=Math.floor(n/a)*b
+    n=Math.floor(n/a)*b+(n%a)
   }
-  let res=cola.reduce((i,j)=>i+j)
-  let remain=remainCola.reduce((i,j)=>i+j)
-  let lastCola=cola.at(-1)+remain
-  if(lastCola>a) Math.floor(lastCola/a)
-  console.log(res);
   return res
 }
 
