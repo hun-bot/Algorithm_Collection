91ca1b59bfe2e1b1d000f2e6a1aec90cc9ac78f0
pydevjeong <njh3421@gmail.com>
2022-12-28 19:30:43 +0900

    Solved

--- LeetCode_Easy/724_FindPivot.js
+++ LeetCode_Easy/724_FindPivot.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-    
+    let leftSum=0
+    let rightSum=0
+    let total=nums.reduce((i,j)=>i+j,0)
+    for(let i=0; i < nums.length; i++) {
+        rightSum=total-nums[i]-leftSum
+        if(rightSum===leftSum) return i
+        leftSum+=nums[i]
+    }
+    return -1
 };
+
+pivotIndex([1,7,3,6,5,6])
