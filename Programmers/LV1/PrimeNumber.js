7567ea52730f8c28c10fce01bd383d153ea53da4
Jeong Hun Nam <njh3421@gmail.com>
2023-01-02 16:59:09 +0900

    Lv1 PrimeNumber Solving

---
+++ Programmers/LV1/PrimeNumber.js
@@ -0,0 +1,9 @@
+// 소수 찾기 https://school.programmers.co.kr/learn/courses/30/lessons/12921
+function solution(n) {
+  let arr=Array(n+1).fill('prime').fill('not',0,2)
+  for(let i=2;i*i<=n;i++){
+    if(arr[i])
+  }
+}
+
+solution(10)
