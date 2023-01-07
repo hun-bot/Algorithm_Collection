cc0746ee0948df30051f951e76b1860039ab6a55
pydevjeong <njh3421@gmail.com>
2023-01-08 00:22:52 +0900

    Lv1 Solved

--- Programmers/LV1/PrimeNumber.js
+++ Programmers/LV1/PrimeNumber.js
@@ -1,9 +1,21 @@
 // 소수 찾기 https://school.programmers.co.kr/learn/courses/30/lessons/12921
 function solution(n) {
-  let arr=Array(n+1).fill('prime').fill('not',0,2)
-  for(let i=2;i*i<=n;i++){
-    if(arr[i])
+  let count=0
+  const sqrt=parseInt(Math.sqrt(n))
+  const arr=Array(n+1).fill(true).fill(false,0,2)
+  for(let i=2;i<=sqrt;i++){
+    if(arr[i]){
+      for (let j = i*i; j <= n; j+=i) {
+        console.log(j);
+        arr[j]=false
+      }
+    }
   }
+
+  for (let i = 2; i < arr.length; i++) {
+    if(arr[i]) count++
+  }
+  return count
 }
 
-solution(10)
+solution(100)
