f6ea7dc33b14b4df5da40ec4ff24c1de2ff07623
pydevjeong <njh3421@gmail.com>
2023-01-07 22:56:23 +0900

    Lv1 Solved

--- Programmers/LV1/Phonekemon.js
+++ Programmers/LV1/Phonekemon.js
@@ -1,1 +1,9 @@
 /* 폰켓몬 https://school.programmers.co.kr/learn/courses/30/lessons/1845 */
+
+function solution(nums) {
+  let set=new Set(nums)
+  let half=nums.length/2
+  return set.size > half ? half :set.size  
+}
+
+solution([3,1,2,3])
