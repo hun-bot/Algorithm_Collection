14b6dd2e3a3cd46fcb81cd451280a84739c1ee08
Jeong Hun Nam <njh3421@gmail.com>
2023-01-19 16:36:27 +0900

    fix solution

--- Programmers/LV1/Rectangle.js
+++ Programmers/LV1/Rectangle.js
@@ -1,16 +1,13 @@
 function solution(sizes) {
-  sizes=sizes.map((e)=>e.sort((a,b)=>a-b))
-  let max=[]
-  let min=[]
-  sizes.forEach((i)=>{
-    max.push(i[0])
-    min.push(i[1])
+  let w=0
+  let h=0
+  sizes.forEach((s)=>{
+    let [min,max]=s.sort((a,b)=>a-b)
+    if(max>w) w=max
+    if(min>h) h=min
   })
-  max.sort((a,b)=>a-b)
-  min.sort((a,b)=>a-b)
-  const res=max[max.length-1]*min[min.length-1]
-  return res
+  return w*h
 }
 
-// solution([[60, 50], [30, 70], [60, 30], [80, 40]])
-solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]])
+solution([[60, 50], [30, 70], [60, 30], [80, 40]])
+// solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]])
