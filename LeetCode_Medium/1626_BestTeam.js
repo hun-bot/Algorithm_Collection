994ab41fe66baf136fba020ec36d939b182a47b1
pydevjeong <njh3421@gmail.com>
2023-01-31 21:40:08 +0900

    LV1 -> LV2 move

---
+++ LeetCode_Medium/1626_BestTeam.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} scores
+ * @param {number[]} ages
+ * @return {number}
+ */
+var bestTeamScore = function(scores, ages) {
+  const helper=()=>{
+
+  }
+    for (let i = 0; i < scores.length; i++) {
+      
+    }
+};
+
+bestTeamScore([1,3,5,10,15],[1,2,3,4,5])
+bestTeamScore([4,5,6,5], [2,1,2,1])
