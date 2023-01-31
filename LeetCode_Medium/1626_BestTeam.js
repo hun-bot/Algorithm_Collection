bfbb7f89e2dda8b72d0a466506e8806a3573600d
pydevjeong <njh3421@gmail.com>
2023-02-01 00:29:25 +0900

    Sovling

--- LeetCode_Medium/1626_BestTeam.js
+++ LeetCode_Medium/1626_BestTeam.js
@@ -4,13 +4,20 @@
  * @return {number}
  */
 var bestTeamScore = function(scores, ages) {
-  const helper=()=>{
-
-  }
-    for (let i = 0; i < scores.length; i++) {
-      
+  let save={}
+  for (let i = 0; i < ages.length; i++) {
+    if(!Object.hasOwn(save,ages[i])){
+      save[ages[i]]=scores[i]
     }
+    else{
+      if(save[ages[i]]<scores[i]) save[ages[i]]=scores[i]
+      else if(save[ages[i]]>scores[i]) continue
+      else save[ages[i]]+=scores[i]
+    }
+  }
+  
 };
 
-bestTeamScore([1,3,5,10,15],[1,2,3,4,5])
-bestTeamScore([4,5,6,5], [2,1,2,1])
+// bestTeamScore([1,3,5,10,15],[1,2,3,4,5])
+// bestTeamScore([4,5,6,5], [2,1,2,1])
+bestTeamScore([1,2,3,5],[8,9,10,1])
