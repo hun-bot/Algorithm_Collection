e8682110b92f03c5d5e4877434c45e579d157baa
pydevjeong <njh3421@gmail.com>
2023-02-11 19:46:40 +0900

    CodeWars

--- solving.js
+++ solving.js
@@ -1,24 +1,11 @@
-function solution(answers) {
-  const ex1 = [1, 2, 3, 4, 5];
-  const ex2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  const ex3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-  let ans = [0, 0, 0];
-  let res = [];
-  answers.forEach((i, idx) => {
-    if (i === ex1[idx]) {
-      ans[0] += 1;
-    }
-    if (i === ex2[idx]) {
-      ans[1] += 1;
-    }
-    if (i === ex3[idx]) {
-      ans[2] += 1;
-    }
-  });
-  for (let i = 0; i < ans.length; i++) {
-    if (ans[i]!==0 && Math.max(...ans) === ans[i]) {
-      res.push(i + 1);
-    }
+function abbrevName(name){
+  let temp=name.split(' ')
+  let res=''
+  for (let i = 0; i < temp.length; i++) {
+    res+=temp[i][0].toUpperCase()
+    res+="."
   }
-  return res;
+  return res.slice(0,-1)
 }
+
+abbrevName("Sam Harris")
