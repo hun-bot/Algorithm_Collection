f53a2ac9696866f43a2df23c69cf6e2caa962b5e
pydevjeong <njh3421@gmail.com>
2023-02-25 16:18:59 +0900

    Update new Solution

--- LeetCode_Easy/121_Stock.js
+++ LeetCode_Easy/121_Stock.js
@@ -8,5 +8,15 @@ var maxProfit = function(prices) {
   }
   return max
 };
+//nice solution
+var maxProfit = function(prices) {
+  let min = prices[0], profit = 0;
+
+  for (const x of prices) {
+    min = Math.min(min, x), profit = Math.max((x - min), profit);
+  }
+
+  return profit;
+};
 
 maxProfit([2,4,1])
