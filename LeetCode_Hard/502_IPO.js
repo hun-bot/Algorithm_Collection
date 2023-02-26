abc30220b105d7043255224d2ac92214ac73e25b
pydevjeong <njh3421@gmail.com>
2023-02-26 18:19:12 +0900

    Passed Example

--- LeetCode_Hard/502_IPO.js
+++ LeetCode_Hard/502_IPO.js
@@ -11,23 +11,32 @@ var findMaximizedCapital = function (k, w, profits, capital) {
   profits.sort((a, b) => a - b);
   capital.sort((a, b) => a - b);
   while (i < k) {
-    if (w === 0) {
-      profit += profits[0];
-    }
+    if (w === 0 && profit===0) profit += profits[0];
     if (profit>0) {
       let idx = capital.lastIndexOf(profit);
       if(idx!==-1){
-        capital[idx] = "used";
+        capital[idx] = null;
         profit += profits[idx];
-        profits.splice(idx,1,'used')
+        i++;
+        profits.splice(idx,1,null)
       }
       else{
-        
+        let maxCapital=Math.max(...capital)
+        // console.log(maxCapital);
+        if(maxCapital<=profit){
+          let maxIdx=capital.indexOf(maxCapital)
+          capital[maxIdx] = null;
+          profit+=profits[maxIdx]
+          i++;
+          profits.splice(idx,1,null)
+        }
       }
       i++;
     }
   }
-  console.log(capital, profits);
+  // console.log(capital, profits, profit);
+  return profit
 };
 
-findMaximizedCapital(2, 0, [1, 2, 3], [0, 1, 1]);
+findMaximizedCapital(3, 0, [1, 2, 3], [0, 1, 2]);
+// findMaximizedCapital(2, 0, [1, 2, 3], [0, 1, 1]);
