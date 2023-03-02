e6096544f6ee32c8618b7fd8aeb1ed396f50ea58
Jeong Hun Nam <njh3421@gmail.com>
2023-03-02 23:40:50 +0000

    test

--- solving.js
+++ solving.js
@@ -1,11 +1,6 @@
-function abbrevName(name){
-  let temp=name.split(' ')
-  let res=''
-  for (let i = 0; i < temp.length; i++) {
-    res+=temp[i][0].toUpperCase()
-    res+="."
-  }
-  return res.slice(0,-1)
+function descendingOrder(n){
+  if(n===0) return n
+  return parseInt(n.toString().split('').sort((a,b)=>b-a).join(''))
 }
 
-abbrevName("Sam Harris")
+descendingOrder(42145)
