72178b5c2698f9f886dd2088d4a84040fe8ab76f
Jeong Hun Nam <njh3421@gmail.com>
2023-03-03 00:54:10 +0900

    Easy Solution

--- CodeWars/Your_order.js
+++ CodeWars/Your_order.js
@@ -9,3 +9,9 @@ function order(words){
   return res
 }
 order("is2 Thi1s T4est 3a")
+
+function order(words){
+  return words.split(' ').sort(function(a, b){
+      return a.match(/\d/) - b.match(/\d/);
+   }).join(' ');
+}  
