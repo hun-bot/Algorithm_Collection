bb24909f23d0322c96f82ee12a786ab734271b3a
pydevjeong <njh3421@gmail.com>
2023-03-06 23:44:39 +0900

    Leetcode 72 solving

---
+++ LeetCode_Hard/72_EditDistance.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function(word1, word2) {
+  let word1Len=word1.length
+  let word2Len=word2.length
+  for (let i = 0; i < word1Len; i++) {
+    
+  }
+};
+
+minDistance("horse","ros")
