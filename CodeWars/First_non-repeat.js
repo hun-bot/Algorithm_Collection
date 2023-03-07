50fdf21f7f7e5a4460581fd54bd626db2857954a
Jeong Hun Nam <njh3421@gmail.com>
2023-03-08 00:14:39 +0900

    expected 'G' to equal ',' (error)

--- CodeWars/First_non-repeat.js
+++ CodeWars/First_non-repeat.js
@@ -1,17 +1,25 @@
 function firstNonRepeatingLetter(s) {
-  if(s.length===1 || !s) return s
-  let obj={}
-  for (const i of s) {
-    obj[i]=(obj[i]||0)+1
+  if(s.length===1) return s
+  if(!s) return ''
+  let C=''
+  let obj=new Map()
+  for (let i of s) {
+    if(i.match(/[A-Z]/g)) C=i.toLowerCase()
+    if(obj.has(C)){
+      // obj.set(i,(obj.get(i)||0)+1)
+      obj.set(C,(obj.get(i)||0)+1)
+    }
+    obj.set(i,(obj.get(i)||0)+1)
   }
-  console.log(obj);
-  for (let i = 0; i < s.length; i++) {
-    if(obj[s[i]]===1){
-      console.log(s[i]);
+  for (let i = 0; i < obj.size; i++) {
+    if([...obj.values()][i]===1){
+      // console.log([...obj.keys()][i]);s
+      return [...obj.keys()][i]
     }
   }
+  return ''
 }
 
-firstNonRepeatingLetter("moonmen")
-firstNonRepeatingLetter("stress")
-firstNonRepeatingLetter("sTreSS")
+// firstNonRepeatingLetter("moonmen")
+firstNonRepeatingLetter("Gaag")
+// firstNonRepeatingLetter("sTreSS")
