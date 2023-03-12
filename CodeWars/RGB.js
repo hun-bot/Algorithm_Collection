2fc11069945c70f48dffd84978e570f060aab81e
pydevjeong <njh3421@gmail.com>
2023-03-12 23:37:43 +0900

    Solved

--- CodeWars/RGB.js
+++ CodeWars/RGB.js
@@ -1,5 +1,12 @@
 function rgb(r, g, b){
-  
+  function rgb(r, g, b){
+    const toHex=(num)=>{
+      if(num<0) return "00"
+      if(num>255) return "FF"
+      return ("0"+(Number(num).toString(16))).slice(-2).toUpperCase()
+    }
+      return toHex(r)+toHex(g)+toHex(b)
+  }
 }
 
 rgb(255,255,255)
