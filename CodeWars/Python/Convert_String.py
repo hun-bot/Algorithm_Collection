991191ab811878795f0cc455fef2b03302053647
Jeong Hun Nam <njh3421@gmail.com>
2023-03-15 21:57:11 +0900

    Other Solutions

--- CodeWars/Python/Convert_String.py
+++ CodeWars/Python/Convert_String.py
@@ -11,3 +11,13 @@ def to_camel_case(text):
     return new_str
   
 to_camel_case("the_Stealth-Warrior")
+
+def to_camel_case(text):
+    removed = text.replace('-', ' ').replace('_', ' ').split()
+    if len(removed) == 0:
+        return ''
+    return removed[0]+ ''.join([x.capitalize() for x in removed[1:]])
+  
+import re
+def to_camel_case(text):
+    return re.sub('[_-](.)', lambda x: x.group(1).upper(), text)
